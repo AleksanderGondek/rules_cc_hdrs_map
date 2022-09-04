ef6f3991411e99431d473e2a9c6e47dc8476cfb7
Aleksander Gondek <gondekaleksander@protonmail.com>
2022-09-04 10:49:32 +0200

    Example: add multiple hdrs mappings

--- examples/tolkien/reuel/ronald/john/include/riddle-two.hpp
+++ examples/tolkien/reuel/ronald/john/include/riddle-two.hpp
@@ -1,6 +1,6 @@
 #ifndef RIDDLE_TWO_HPP
 #define RIDDLE_TWO_HPP
 
-const std::string RIDDLE_LINE_TWO = "All that is gold does not glitter,";
+const std::string RIDDLE_LINE_TWO = "Not all those who wander are lost;";
 
 #endif
