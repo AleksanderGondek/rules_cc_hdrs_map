011d3adfdc992c1c83dc8f98635a572dad86eb89
Aleksander Gondek <gondekaleksander@protonmail.com>
2025-11-21 13:44:45 +0000

    fix(examples): libuuid is no longer dynamically taken from nix
    This change-set removes the script, that automatically
    populates the examples/libuuid with appropriate version
    of libuuid.so in favor of having that small binary
    file (22K) checked in.
    
    This is done due to the issues of retaining the
    behavior while run in the BCR automated tests.

--- default.nix
+++ default.nix
@@ -25,59 +25,6 @@
     zlib
   ]);
 
-  # This is required to ensure that the
-  # symbols libuuid needs, are compatbile
-  # with the sysroot-provided libs of
-  # Bazel toolchain (they are old)
-  libuuid_dev = nixpkgs.stdenv.mkDerivation {
-    name = "libuuid_dev";
-    version = "1.3.0@linux2.31.1";
-
-    src = builtins.fetchurl {
-      url = "https://cache.nixos.org/nar/039ykinvrwg3qxhpk6mcasbi4b0lfhyag9nxv6jagrja5jwwnjn6.nar.xz";
-      sha256 = "039ykinvrwg3qxhpk6mcasbi4b0lfhyag9nxv6jagrja5jwwnjn6";
-    };
-
-    nativeBuildInputs = [nixpkgs.xz nixpkgs.nix];
-
-    unpackPhase = ''
-      cat $src | xz -dc | nix-store --restore ./tmp
-      mkdir -p $out/include
-      mv ./tmp/include/uuid $out/include/uuid
-    '';
-
-    dontPatch = true;
-    dontConfigure = true;
-    dontBuild = true;
-    dontCheck = true;
-    dontInstall = true;
-    dontFixup = true;
-  };
-  libuuid_lib = nixpkgs.stdenv.mkDerivation {
-    name = "libuuid_lib";
-    version = "1.3.0@linux2.31.1";
-
-    src = builtins.fetchurl {
-      url = "https://cache.nixos.org/nar/00sxgnrjwqcbpa7f1z4gq55gfcwfv6407xl5rq81cr0l12d4hr9n.nar.xz";
-      sha256 = "00sxgnrjwqcbpa7f1z4gq55gfcwfv6407xl5rq81cr0l12d4hr9n";
-    };
-
-    nativeBuildInputs = [nixpkgs.xz nixpkgs.nix];
-
-    unpackPhase = ''
-      cat $src | xz -dc | nix-store --restore ./tmp
-      mkdir -p $out/lib
-      mv ./tmp/lib $out/
-    '';
-
-    dontPatch = true;
-    dontConfigure = true;
-    dontBuild = true;
-    dontCheck = true;
-    dontInstall = true;
-    dontFixup = true;
-  };
-
   # buildFHSEnv to be able to use bazelisk on NixOS
   devShellNixOs =
     (nixpkgs.buildFHSEnv {
@@ -94,10 +41,6 @@
           mkdir -p .bazelisk-bin
           ln -f -s ${nixpkgs.bazelisk}/bin/bazelisk .bazelisk-bin/bazel
           export PATH="$(realpath .bazelisk-bin)/:$PATH"
-
-          # TODO: Retrieve sol via http_archive
-          ln -f -s ${libuuid_dev}/include ./examples/libuuid/include
-          ln -f -s ${libuuid_lib}/lib ./examples/libuuid/lib
         EOF
 
         exec bash --rcfile $shellHooksPath
@@ -119,10 +62,6 @@
       mkdir -p .bazelisk-bin
       ln -f -s ${nixpkgs.bazelisk}/bin/bazelisk .bazelisk-bin/bazel
       export PATH="$(realpath .bazelisk-bin)/:$PATH"
-
-      # TODO: Retrieve sol via http_archive
-      ln -f -s ${libuuid_dev}/include ./examples/libuuid/include
-      ln -f -s ${libuuid_lib}/lib ./examples/libuuid/lib
     '';
   };
 in {
